96b6cde0edf54ce0a95a7dfe5b3079a054db0fa4
Rob Brackett <rob@robbrackett.com>
2017-08-21 16:57:05 -0700

    Convert errorCode -> error_code in source_metadata

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801192038) do
+ActiveRecord::Schema.define(version: 20170821231916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
